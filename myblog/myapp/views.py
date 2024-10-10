3b274efee6bbad9481f38ec3ac1a72cc492c010d
jenstem <jennifer.stemkowski@gmail.com>
2024-10-10 11:53:19 -0400

    Pass variable list through django block in home.html

--- myblog/myapp/views.py
+++ myblog/myapp/views.py
@@ -3,11 +3,12 @@ from django.shortcuts import render
 
 def home_screen(request):
     print(request.headers)
-
-
-    context = {
-        "some_string": "This is some string 2",
-    }
+    context = {}
+    list_var = []
+    list_var.append("Value 1")
+    list_var.append("Value 2")
+    list_var.append("Value 3")
+    context["list_var"] = list_var
 
 
     return render(request, 'personal/home.html', context)
