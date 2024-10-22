4c824e46694bf2dd7d63cc6bf799238f9d308dcc
jenstem <jennifer.stemkowski@gmail.com>
2024-10-22 16:00:58 -0400

    Add styling to detail template

--- myblog/blog/views.py
+++ myblog/blog/views.py
@@ -5,4 +5,4 @@ from .models import Post
 def detail(request, slug):
     post = get_object_or_404(Post, slug=slug)
 
-    return render(request, 'myblog/detail.html', {'post': post})
+    return render(request, 'blog/detail.html', {'post': post})
