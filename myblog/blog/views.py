1b6e26a0d377ec402fc2c107e4ea176799c767d5
jenstem <jennifer.stemkowski@gmail.com>
2024-10-22 16:25:45 -0400

    Set up initial comment form

--- myblog/blog/views.py
+++ myblog/blog/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import get_object_or_404, render
+from .forms import CommentForm
 from .models import Post
 
 
 def detail(request, slug):
     post = get_object_or_404(Post, slug=slug)
-
-    return render(request, 'blog/detail.html', {'post': post})
+    form = CommentForm()
+    return render(request, 'blog/detail.html', {'post': post, 'form': form})
