da144c2ca2f6d047c3818af0e43fa4279cb9bd36
jenstem <jennifer.stemkowski@gmail.com>
2024-11-04 15:18:56 -0500

    Add a robots.txt file

--- myblog/myapp/views.py
+++ myblog/myapp/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 from blog.models import Post
 
 
@@ -31,3 +32,20 @@ def about(request):
         HttpResponse: The rendered HTML page for the about section.
     '''
     return render(request, 'personal/about.html')
+
+
+def robots_txt(request):
+    '''
+    Custom view function that returns a robots.txt file.
+
+    Args:
+        request (HttpRequest): The HTTP request object.
+
+    Returns:
+        HttpResponse: A robots.txt file that disallows all search engine crawlers from
+    '''
+    text = [
+        "User-agent: *",
+        "Disallow: /admin/",
+    ]
+    return HttpResponse("\n".join(text), content_type="text/plain")
--- myblog/myblog/urls.py
+++ myblog/myblog/urls.py
@@ -23,10 +23,13 @@ from myapp.views import home_screen, about
 
 from .sitemaps import CategorySitemap, PostSitemap
 
+from myapp.views import home_screen, about, robots_txt
+
 sitemaps = {'category': CategorySitemap, 'post': PostSitemap}
 
 urlpatterns = [
     path('sitemap.xml', sitemap, {'sitemaps': sitemaps}),
+    path('robots.txt', robots_txt, name='robots_txt'),
     path('admin/', admin.site.urls),
     path('about/', about, name='about'),
     path('', include('blog.urls')),
